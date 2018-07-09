981b203cf720a71967bdbd469051eda8fbccf92a
The WA <a4ron5@yandex.ru>
2018-07-09 23:24:37 +0500

    gh page

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+const URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
 const API_KEY = '608c8ccfcc607c63abf75262bd89d33d';
 
 const fetchWeather = async (city) => {
