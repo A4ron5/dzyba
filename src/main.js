48d58d5355d395bd1f6cf9facf6210b1f4055ba2
The WA <a4ron5@yandex.ru>
2018-07-09 19:35:08 +0500

    fix

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { Weather } from './features/weather'
+import { Weather } from './features/weather';
 
-export const Main = (props) => {
-  return (
-    <Provider store={store}>
-      <Weather />
-    </Provider>
-  )  
-}
+export const Main = (props) => (
+  <Provider store={store}>
+    <Weather />
+  </Provider>
+);
