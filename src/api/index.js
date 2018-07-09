a713ff62b081c0d9c9e4c85ecc4ed22874b71557
The WA <a4ron5@yandex.ru>
2018-07-09 19:34:14 +0500

    api fix

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 const URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
 const API_KEY = '608c8ccfcc607c63abf75262bd89d33d';
 
-const fetchWeather = (city) => {
-  return axios.get(`${URL}${city}&appid=${API_KEY}`);
+const fetchWeather = async (city) => {
+  const data = await axios.get(`${URL}${city}&units=metric&appid=${API_KEY}`);
+  return data;
 };
 
 export default {
