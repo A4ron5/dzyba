12389226293a155bd82b5d98802f123fae837cfd
The WA <a4ron5@yandex.ru>
2018-07-09 23:37:16 +0500

    Auto stash before merge of "master" and "a4ron5/master"

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+<<<<<<< Updated upstream
 const API_KEY = '608c8ccfcc607c63abf75262bd89d33d';
+=======
+const API_KEY = '';
+>>>>>>> Stashed changes
 
 const fetchWeather = async (city) => {
   const data = await axios.get(`${URL}${city}&units=metric&appid=${API_KEY}`);
