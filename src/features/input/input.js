1f9febca02391cddc01c564b2b1cdc4e64546304
The WA <a4ron5@yandex.ru>
2018-07-10 15:13:43 +0500

    feat(input): add proptypes

--- src/features/input/input.js
+++ src/features/input/input.js
@@ -1,37 +1,48 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { Button, InputUI, Form } from '../../ui/atom';
 import { fetchWeather } from '../weather/action';
 
 export class InputRaw extends React.Component {
+  static propTypes = {
+    fetchWeather: PropTypes.func.isRequired,
+    handleOpen: PropTypes.func.isRequired,
+    isOpen: PropTypes.bool.isRequired,
+  }
 
   state = {
     city: '',
   }
 
   handleClick = (e) => {
+    const { fetchWeather, handleOpen } = this.props;
+    const { city } = this.state;
     e.preventDefault();
-    this.props.fetchWeather(this.state.city)
-    this.props.handleOpen();
+    fetchWeather(city);
+    handleOpen();
   }
 
   handleChange = (e) => {
     this.setState({
-      city: e.target.value
-    })
+      city: e.target.value,
+    });
   }
 
   render() {
+    const { isOpen } = this.props;
     return (
-      this.props.isOpen ? 
-      (
-        <Form>
-          <InputUI onChange={this.handleChange} />
-          <Button onClick={this.handleClick}>ok</Button>
-        </Form>
-      ) : null
-    )
+      isOpen
+        ? (
+          <Form>
+            <InputUI onChange={this.handleChange} />
+            <Button onClick={this.handleClick}>
+              {'ok'}
+            </Button>
+          </Form>
+        ) : null
+    );
   }
 }
 
-export const Input = connect(null, { fetchWeather })(InputRaw)
+export const Input = connect(null, { fetchWeather })(InputRaw);
